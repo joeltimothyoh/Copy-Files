3844c7164f1edb5f0653a525336ad6e3d9df62ff
Joel Timothy Oh <joeltimothyoh@live.com>
2017-11-28 02:41:04 +0800

    Improved behavior when copying individual files as sources, updated description

--- Copy-Files.ps1
+++ Copy-Files.ps1
@@ -3,13 +3,14 @@
 function Copy-Files {
     <#
     .SYNOPSIS
-    Copy-Files will copy the select folder(s) to specified destination(s).
+    Copy-Files will copy select files and folders to specified destination(s).
     
     .DESCRIPTION
-    Copy-Files will copy the contents of each specified folder to the destination using Robocopy.
-    The Robocopy /MIR parameter is left out by default. Use /MIR to mirror copy, which includes purging of the files in destination not found in source.
-    The Robocopy /XF parameter excludes copying of files with a matching name or extension after the parameter.
-    The Robocopy /XD parameter excludes copying of directories with a matching directory name or extension after the parameter.
+    Copy-Files will copy select files and folders to specified destination(s) using Robocopy.
+    The Robocopy /MIR parameter is left out by default. /MIR specifies robocopy to make a mirror copy of the source directory in the destination directory. Use /MIR with caution as it will delete files in the destination not present in the source. 
+    The Robocopy /XF parameter excludes files with matching name from all operations.
+    The Robocopy /XD parameter excludes folders with a matching name from all operations.
+    Files that are specified under sources will not have /MIR, /E, /S passed along with them, so as to prevent folders within the same directory from being copied as well, a behavior of Robocopy when specific files are copied with any one of those parameters passed.
     Run 'robocopy /?' for usage information.
       
     .EXAMPLE
@@ -23,28 +24,34 @@ function Copy-Files {
 
     # Batch of files / folders to copy
     $sources = @( 
-        #'C:\Users\username\Documents\Project1',
-        #'C:\Users\username\Documents\report.doc',
-        # 'D:\Git\Project1\Repository3'  
+        #'C:\Users\username\Documents\Project1'
+        #'C:\Users\username\Documents\report.doc'
+        #'D:\Git\Project1\Repository3'  
     )
 
     # Destination(s) to copy the batch of files / folders to
     $destinations = @(
-        #'E:\Backup\AllProjects',
-        #'G:\backupfolder\scripts',
+        #'E:\Backup\AllProjects'
+        #'G:\backupfolder\scripts'
         #'\\SERVER1\projects\project1'
     )
 
     # Robocopy copy options
     $robocopy_options = @(
-        #'/MIR', 
-        #'/XF',
-        #'Examplefile.doc',
+        #'/E' 
+        #'/S' 
+        #'/MIR'
+        #'/XX'        
+        #'/XA:H'
+        #'*.docx'
+        #'/XF'
+        #'Examplefile.doc'
         #'*.log'
-        '/XD',              
-        #'nameoffolderstoexclude',
-        #'subfolder1',
+        '/XD'          
+        #'nameoffolderstoexclude'
+        #'subfolder1'
         '*.git'
+        #'/L'
     )
 
     # Get properties of each source specified
@@ -71,30 +78,29 @@ function Copy-Files {
     Write-Output "Items:" $items.Name ""
 
     # Signal Start
-    Write-Host "START`n"  -ForegroundColor Cyan
+    Write-Host "START`n" -ForegroundColor Cyan
 
     # Make a copy of all sources to each destination specified
     foreach ($destination in $destinations)
-    {    
-        Write-Host "Destination: $($destination)" -ForegroundColor Green   
+    {
+        Write-Host "Destination: $($destination)" -ForegroundColor Green
         Write-Host "Robocopy Options: $($robocopy_options)`n" -ForegroundColor Magenta
         foreach ($item in $items)
-        {                            
-            Write-Host "Item: $($item.Name)" -ForegroundColor Yellow  
-            Write-Host "Item Attributes: $($item.Attributes)" -ForegroundColor Yellow      
+        {
+            Write-Host "Item: $($item.Name)" -ForegroundColor Yellow
+            Write-Host "Item Attributes: $($item.Attributes)" -ForegroundColor Yellow
+            Write-Host "Source: $($item.FullName)" -ForegroundColor Yellow
             
-            # Define parameters depending on whether source is a file or folder 
+            # Define parameters depending on whether source is a file or folder
             if ($item.Attributes -match 'Archive') {    # match is used as $item.Attributes returns a string of attributes
-                Write-Host "Source: $($item.DirectoryName)" -ForegroundColor Yellow     
-                $prm = $($item.DirectoryName), $destination, $item.Name + $robocopy_options          
-            } 
+                $prm = $item.DirectoryName, $destination, $item.Name + ($robocopy_options | Where-Object { ($_ -ne '/MIR') -and ($_ -ne '/E') -and ($_ -ne '/S')})      # /MIR, /E, /S are ignored for file sources
+            }
             elseif ($item.Attributes -match 'Directory') {
-                Write-Host "Source: $($item.FullName)" -ForegroundColor Yellow 
                 $prm = $item.FullName, "$($destination)\$($item.Name)" + $robocopy_options
-            } 
+            }
 
             # Execute Robocopy with set parameters
-            Write-Host "Executing:" $cmd $prm -ForegroundColor DarkGray 
+            Write-Host "Executing:" $cmd $prm -ForegroundColor DarkGray
             & $cmd $prm
         }
     }  
